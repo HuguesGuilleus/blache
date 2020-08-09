0d8e09a40c0b4576b9585c0821f83cf56dea3d24
hugues <ghugues@netc.fr>
2020-08-09 17:55:15 +0200

    print version and license

--- main.go
+++ main.go
@@ -5,32 +5,73 @@
 package main
 
 import (
+	"./meta"
 	"./pkg"
 	"flag"
-	"log"
+	"fmt"
+	"os"
 	"time"
 )
 
-func init() {
-	log.SetFlags(log.Lshortfile)
-	log.Println("main()")
-}
-
 func main() {
-	out := blache.NewWriterFile("dist")
-	opt := blache.Option{
-		Out: out,
+	flag.Usage = func() {
+		fmt.Println("Usage: $ blache [OPTION ...] input")
+		fmt.Println()
+		fmt.Println("  input is a directory that contain minecraft regions (*.mca)")
+		flag.PrintDefaults()
 	}
+	out := blache.NewWriterFile("dist")
 	flag.Var(out, "out", "The output Directory")
+	opt := blache.Option{Out: out}
 	flag.IntVar(&opt.CPU, "cpu", 0, "The number of core used, zero is for all core.")
+	version := flag.Bool("version", false, "Print the version and exit")
 	flag.Parse()
 	opt.In = blache.NewReaderFile(flag.Arg(0))
 
+	if *version {
+		fmt.Println("Blache")
+		fmt.Println()
+		fmt.Println("\tGit: ", meta.Git)
+		fmt.Println("\tDate:", meta.Date)
+		fmt.Println()
+		fmt.Println(license)
+		return
+	}
+
 	defer func(before time.Time) {
-		log.Println("[DURATION]", time.Since(before).Round(time.Millisecond*10))
+		fmt.Println("[DURATION]", time.Since(before).Round(time.Millisecond*10))
 	}(time.Now())
 
 	for err := range opt.Gen() {
-		log.Println(err)
+		fmt.Fprintln(os.Stderr, err)
 	}
 }
+
+const license = `BSD 3-Clause License
+Copyright (c) 2020, Hugues GUILLEUS
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.`
